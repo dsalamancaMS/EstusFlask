c665ab58d690c0fb2b6fc6e43369987cf8623f8f
Denis Salamanca <desalama@microsoft.com>
2018-11-29 08:36:27 -0600

    os envV support

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask import render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def estus_flask():
-    message = "Hello Azure"
+    message = "Hello from " + os.getenv("HOSTNAME")
     return render_template("index.html", message=message, language="Python")
 
